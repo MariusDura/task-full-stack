b1b82996ad507cd771ca7a100549d84993fd8cc4
Marius Dura <termosroviukas@gmail.com>
2019-01-29 10:45:30 +0200

    Company and Address seeders fix, to avoid problems with user.

--- backend-api/database/seeds/CompaniesTableSeeder.php
+++ backend-api/database/seeds/CompaniesTableSeeder.php
@@ -12,6 +12,6 @@ class CompaniesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Company::class, 20)->create();
+        factory(Company::class, 50)->create();
     }
 }
